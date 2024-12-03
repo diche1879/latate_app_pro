b67d5ada3d6921dd873aac0ce748cf306323e82c
diche79 <diche79tg@gmail.com>
2024-12-03 12:05:24 +0100

    commit_3.2

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<?php include "latate_private/inc/head.php"; ?>
+    <link rel="stylesheet" href="css/bulma.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style_dos.css">
 </head>
+
 <body>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+        <a class="logo" href="index.php?view=home">LaTaTe</a>
+    </nav>
     <h1>Welcome to Latate-Pro</h1>
     <a href="latate_private">Private</a>
     <a href="latate_clients">Clients</a>
-    
 </body>
+
 </html>
