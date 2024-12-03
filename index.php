363e24497b341a6f2a9781d38641c181715c2b81
diche79 <diche79tg@gmail.com>
2024-12-03 12:37:52 +0100

    commit_3.3
    aaaaajuste index principal

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 <html lang="en">
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LaTaTe Pro</title>
     <link rel="stylesheet" href="css/bulma.min.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/style_dos.css">
@@ -10,10 +13,18 @@
 <body>
     <nav class="navbar" role="navigation" aria-label="main navigation">
         <a class="logo" href="index.php?view=home">LaTaTe</a>
-    </nav>
-    <h1>Welcome to Latate-Pro</h1>
-    <a href="latate_private">Private</a>
-    <a href="latate_clients">Clients</a>
-</body>
 
-</html>
+        <div class="navbar-end">
+            <div class="navbar-item">
+                <div class="buttons">
+                    <a href="latate_clients" class="button is-rounded out">Clients</a>
+                    <a href="latate_private" class="button is-rounded in">Private</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <header class="prime">
+        <h1>Welcome to Latate-Pro</h1>
+    </header>
+
+</body>
