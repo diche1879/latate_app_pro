7b4c55d8bb0df76014564ab5a76dc4ab55ba41e2
diche79 <diche79tg@gmail.com>
2024-12-03 11:56:02 +0100

    commit_3
    arreglar rutas

--- latate_clients/index.php
+++ latate_clients/index.php
@@ -2,12 +2,7 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/bulma.min.css">
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/style_dos.css">
-    <title>Client Access</title>
+    <?php include "../latate_private/inc/head.php"; ?>
 </head>
 
 <body>
